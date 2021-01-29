0e7b2c9d0bcd866db8aa6d2ec6f6c5109abf0c40
htanaka <pirosukke@gmail.com>
2021-01-29 20:29:37 +0900

    fixed question set generating logic

--- hyakushu-web/src/sql/question_sets/select_questions_for_new_set.sql
+++ hyakushu-web/src/sql/question_sets/select_questions_for_new_set.sql
@@ -1,19 +1,33 @@
 with
-mostly_misanswered_questions as (
+correct_answer_rates as (
     select
         waka_id
-        ,count(*) as count
+        ,count(*) filter (where answered_correctly) as correct_count
+        ,count(*) as total_count
+        ,(count(*) filter (where answered_correctly)::float) / count(*)::float as rate
     from answer_logs
     where player_id = $1
-    and not answered_correctly
     group by waka_id
-    order by count desc
+)
+,wakas_by_answer_rate as (
+    select
+        w.waka_id
+        ,coalesce(car.correct_count, 0)::float / coalesce(total_count, 1)::float as rate
+    from wakas w
+    left join correct_answer_rates car
+        on w.waka_id = car.waka_id
+    order by rate asc
     limit ($2 / 2)::int
 )
-,random_questions as (
+,random_wakas as (
     select
         waka_id
     from wakas
+    where waka_id not in (
+        select
+            waka_id
+        from wakas_by_answer_rate
+    )
     order by random()
     limit $2
 )
@@ -26,10 +40,10 @@ from wakas
 where waka_id in (
     select
         waka_id
-    from mostly_misanswered_questions
+    from wakas_by_answer_rate
     union
     select
         waka_id
-    from random_questions
+    from random_wakas
 )
 limit $2
